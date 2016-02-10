43548d672531f9edb672da405b48881ea2c95903
regison <reginald.augustin@gmail.com>
2016-02-09 19:23:56 -0500

    [RAU] add first function "connectUserWithCredentials"

--- LOG660/FACADE/WebFlixFacade.cs
+++ LOG660/FACADE/WebFlixFacade.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LOG660.DATA;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,36 @@ namespace LOG660.FACADE
 {
     public class WebFlixFacade
     {
+        private static MovieEntity _entityWebFlixMgr;
+        
+        //Singleton de  l'objet de la BD
+        public static MovieEntity getInstance
+        {
+            get
+            {
+                if (_entityWebFlixMgr == null)
+                   _entityWebFlixMgr = new MovieEntity();
+
+                return _entityWebFlixMgr;
+
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="usercode"></param>
+        /// <param name="userPassword"></param>
+        /// <returns></returns>
+        public USAGER connectUserWithCredentials(string usercode, string userPassword)
+        {
+            if (!String.IsNullOrEmpty(usercode) && !String.IsNullOrEmpty(userPassword))        
+                return _entityWebFlixMgr.USAGERs.FirstOrDefault(u => u.COURRIEL.ToLower().Equals(usercode.ToLower()) && u.MOTPASSE.Equals(userPassword));
+         
+            return null;
+        }
+
+
+
     }
 }
