4857fb08bde914414b13a386d5f83080e924e756
jeph0309 <jp33_83@hotmail.com>
2016-02-11 15:13:52 -0500

    test

--- LOG660/Program.cs
+++ LOG660/Program.cs
@@ -16,7 +16,7 @@ namespace LOG660
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); // test
         }
     }
 }
