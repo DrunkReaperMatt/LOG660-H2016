e7f3e80d4a655c83414805b5fff889970b0dc485
regison <reginald.augustin@gmail.com>
2016-02-23 17:27:17 -0500

    [RAU] Fixing references into UNITTEST PROJECT

--- LOG660UnitTest/UnitTest1.cs
+++ LOG660UnitTest/UnitTest1.cs
@@ -10,8 +10,8 @@ namespace LOG660UnitTest
         [TestMethod]
         public void TestUserConnexion()
         {
-            var web = WebFlixFacade.getInstance;            
-            
+            var web = WebFlixFacade.getInstance.getEntity;
+
             var u = WebFlixFacade.connectUserWithCredentials("reginald.augustin@gmail.com", "pwd");
             Assert.AreEqual(u, null);
         }
